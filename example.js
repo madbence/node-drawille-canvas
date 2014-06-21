16c0d6af6d672ed355a4a4fe88e208f3f922ce66
Bence Dányi <madbence@gmail.com>
2014-06-21 22:01:24 +0200

    Add better example

--- example.js
+++ example.js
@@ -1,26 +1,38 @@
 var Canvas = require('./');
 
-var c = new Canvas(160, 160);
+var n = 20;
+var a = 50;
+var w = 80;
+var t = 2;
+var pi = Math.PI;
+var pi2 = pi/2;
+var sin = Math.sin;
+var cos = Math.cos;
+
+var c = new Canvas(w*2, w*2);
 
 function draw() {
-  var now = Date.now();
+  var now = Date.now()/1000;
   c._canvas.clear();
   c.save();
-  c.rotate(now/1000*360/5);
-  c.translate(80, 80);
-  c.fillRect(-10, -10, 20, 20);
-  c.beginPath();
-  c.moveTo(0,0);
-  c.lineTo(20,0);
-  for(var i = 0; i < 11; i++) {
-    c.lineTo(Math.cos(Math.PI/20*i)*20, Math.sin(Math.PI/20*i)*20);
+  c.translate(w, w);
+  for(var i = 1; i < n; i++) {
+    var r = i*(w/n);
+    c.beginPath();
+    c.moveTo(-r, 0);
+    var tt = now*pi/t;
+    var p = (sin(tt-pi*(cos(pi*i/n)+1)/2)+1)*pi2;
+    for(var j = 0; j < a; j++) {
+      var ca = pi*j/(a-i);
+      if(p > ca) {
+        c.lineTo(-cos(ca)*r, -sin(ca)*r);
+      } else {
+        c.lineTo(-cos(p)*r, -sin(p)*r);
+      }
+    }
+    c.stroke();
   }
-  c.lineTo(0,0)
-  c.stroke();
   c.restore();
-
-  c.fillRect(10, 10, 20, 20);
-  c.clearRect(15, 15, 10, 10);
   console.log(c._canvas.frame());
 }
 
